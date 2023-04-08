1f4fb3897137ef6a52ad46b0c6ce4822de7a7a61
Romaomoura <c.romaomoura@gmail.com>
2023-04-08 19:06:17 -0300

    Subindo projeto Inicial

---
+++ src/test/java/com/romoura/sgfp/SgfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romoura.sgfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
